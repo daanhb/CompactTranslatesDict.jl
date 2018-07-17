152375fb1b577847f462cc5f95066996f2908565
vincentcp <vincent.coppe@gmail.com>
2018-07-17 23:22:55 +0200

    add and clone

--- deps/build.jl
+++ deps/build.jl
@@ -1,4 +1,5 @@
 Pkg.clone("https://github.com/vincentcp/CardinalBSplines.git")
 Pkg.clone("https://github.com/daanhb/BasisFunctions.jl.git","extract")
+Pkg.add("BasisFunction")
 Pkg.build("BasisFunction")
 Pkg.clone("https://github.com/daanhb/Domains.jl.git")
