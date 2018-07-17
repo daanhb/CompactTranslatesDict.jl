d5a419f5c9abd3de3f0fd435cbb874d4c130930e
vincentcp <vincent.coppe@gmail.com>
2018-07-18 00:00:48 +0200

    try without LinearAlgebra

--- deps/build.jl
+++ deps/build.jl
@@ -1,4 +1,4 @@
 Pkg.clone("https://github.com/vincentcp/CardinalBSplines.git")
-Pkg.clone("https://github.com/vincentcp/LinearAlgebra.git")
+# Pkg.clone("https://github.com/vincentcp/LinearAlgebra.jl.git")
 Pkg.clone("https://github.com/daanhb/BasisFunctions.jl.git","extract")
 Pkg.clone("https://github.com/daanhb/Domains.jl.git")
