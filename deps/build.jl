dfe390c331e9c4bcb929825dabc1b2120ccccbc7
vincentcp <vincent.coppe@gmail.com>
2018-07-17 23:05:05 +0200

    add iso clone

--- deps/build.jl
+++ deps/build.jl
@@ -1,4 +1,3 @@
-Pkg.clone("https://github.com/vincentcp/CardinalBSplines.git")
-Pkg.clone("https://github.com/daanhb/BasisFunctions.jl.git")
-Pkg.checkout("BasisFunctions","extract")
-Pkg.clone("https://github.com/daanhb/Domains.jl.git")
+Pkg.add("https://github.com/vincentcp/CardinalBSplines.git")
+Pkg.add("https://github.com/daanhb/BasisFunctions.jl.git","extract")
+Pkg.add("https://github.com/daanhb/Domains.jl.git")
