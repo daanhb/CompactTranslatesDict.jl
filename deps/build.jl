f47ac866f09653be0e3c15391901ffdb66149509
vincentcp <vincent.coppe@gmail.com>
2018-07-18 00:08:39 +0200

    use checkout

--- deps/build.jl
+++ deps/build.jl
@@ -1,4 +1,6 @@
 Pkg.clone("https://github.com/vincentcp/CardinalBSplines.git")
 # Pkg.clone("https://github.com/vincentcp/LinearAlgebra.jl.git")
-Pkg.clone("https://github.com/daanhb/BasisFunctions.jl.git","extract")
+Pkg.clone("https://github.com/daanhb/BasisFunctions.jl.git")
+Pkg.checkout("BasisFunctions","extract")
+Pkg.build("BasisFunctions")
 Pkg.clone("https://github.com/daanhb/Domains.jl.git")
