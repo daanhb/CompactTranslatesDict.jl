80a76eddcc2e8237ff2cbeb91c031e5f672cb304
vincentcp <vincent.coppe@gmail.com>
2018-07-24 11:49:00 +0200

    remove depr code from deps

--- deps/build.jl
+++ deps/build.jl
@@ -1,5 +1,12 @@
-Pkg.clone("https://github.com/vincentcp/CardinalBSplines.git")
-Pkg.clone("https://github.com/daanhb/BasisFunctions.jl.git")
-Pkg.checkout("BasisFunctions", "julia-0.7")
-Pkg.build("BasisFunctions")
-Pkg.clone("https://github.com/daanhb/Domains.jl.git")
+if VERSION < v"0.7-"
+    Pkg.clone("https://github.com/vincentcp/CardinalBSplines.git")
+    Pkg.clone("https://github.com/daanhb/BasisFunctions.jl.git")
+    Pkg.checkout("BasisFunctions", "julia-0.7")
+    Pkg.build("BasisFunctions")
+    Pkg.clone("https://github.com/daanhb/Domains.jl.git")
+else
+    using Pkg
+    Pkg.add("https://github.com/vincentcp/CardinalBSplines.git")
+    Pkg.add("https://github.com/daanhb/Domains.jl.git")
+    Pkg.add("https://github.com/daanhb/BasisFunctions.jl.git#julia-0.7")
+end
